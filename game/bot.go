53545120c309636029cc4c51b24095b55afad05e
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-02 14:32:15 -0400

    Return JSON response after ExecuteAction()

--- game/bot.go
+++ game/bot.go
@@ -8,7 +8,9 @@ type Bot struct {
 	/* add custom struct fields */
 }
 
-func (a *Bot) ExecuteAction(Player *Player, Map *Map) {
+func (a *Bot) ExecuteAction(Player *Player, Map *Map) Action {
 	fmt.Println(Player.Upgrades.Attack)
 	Map.Print()
+
+	return CreateMeleeAttackAction(1, 0)
 }
--- game/main.go
+++ game/main.go
@@ -27,7 +27,9 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	/* decode the url-encoded buffer */
 	data, err := url.QueryUnescape(buf.String())
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Println("Couldn't decode URL encoded data.")
+		return
 	}
 
 	/* remove `data=` from the start of the buffer */
@@ -37,13 +39,27 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	var jsonGame JSONGameInfo
 	err = json.Unmarshal([]byte(data), &jsonGame)
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Println(err)
 		return
 	}
 
-	/* get the next action*/
+	/* get the next action */
 	var game = jsonGame.GameInfo()
-	PlayerBot.ExecuteAction(&game.Player, &game.Map)
+	var action = PlayerBot.ExecuteAction(&game.Player, &game.Map)
+
+	/* encode the action */
+	actionJSON, err := json.Marshal(action.JSONAction())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Failed to encode JSON action.")
+		return
+	}
+
+	/* return the action */
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(actionJSON)
 }
 
 func main() {
